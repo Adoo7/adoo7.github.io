6ce78e4b2d71eb639a05b0ba9bceade87ab5a151
adoo <adoobinsarhan@gmail.com>
2025-02-01 22:43:17 +0300

    replaced files with build files

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import './FooterStyle.css'
-
-export function Footer() {
-  return (
-    <div className='footer'>
-      <span>© 2021 Ahmed Sarhan</span>
-    </div>
-  );
-}
--- src/components/spacer/Spacer.tsx
+++
@@ -1,3 +0,0 @@
-export function Spacer({ height }: { height: number; }) {
-  return <div className='spacer' style={{ height: height + 'em' }} />;
-}
